9cd22e8d7cc6e44a10535b89b5cefcec17990c07
Ko Collins-Sibley <ko@collins-sibley.com>
2021-05-25 12:26:04 -0400

    Initial commit of MERN CRUD test application

---
+++ server/routes/record.js
@@ -0,0 +1,71 @@
+const express = require ("express");
+const ObjectId = require ("mongodb").ObjectId;
+
+const app = express();
+
+// This will help us connect to the database.
+const dbo = require("../db/conn");
+
+// This section will help you get a list of all the records.
+app.get("/record", function (req, res) {
+    let db_connect = dbo.getDb("employees");
+    db_connect
+        .collection("records")
+        .find({})
+        .toArray(function (err, result) {
+            if (err) throw err;
+            res.json(result);
+        });
+});
+
+app.get("/record/:id", function (req, res) {
+    let db_connect = dbo.getDb("employees");
+    var myquery = { _id: ObjectId(req.params.id) };
+    db_connect.collection("records").findOne(myquery, function (err, result) {
+        if (err) throw err;
+        res.json(result);
+    });
+});
+
+app.post("/record/add", function (req, res) {
+    let db_connect = dbo.getDb("employees");
+
+    let newRecord = {
+        person_name: req.body.person_name,
+        person_position: req.body.person_position,
+        person_level: req.body.person_level,
+    };
+
+    db_connect.collection("records").insertOne(newRecord, function (err, res) {
+        if (err) throw err;
+    });
+});
+
+app.post("/update/:id", function (req, res) {
+    let db_connect = dbo.getDb("employees");
+    let myquery = { _id: ObjectId(req.params.id) };
+    let newvalues = {
+        $set: {
+            person_name: req.body.person_name,
+            person_position: req.body.person_position,
+            person_level: req.body.person_level,
+        },
+    };
+
+
+    db_connect
+        .collection("records")
+        .updateOne(myquery, newvalues, function (err, res) {
+            if (err) throw err;
+        });
+});
+
+app.delete("/delete/:id", function (req, res) {
+    let db_connect = dbo.getDb("employees");
+    var myquery = { _id: ObjectId(req.params.id) };
+    db_connect.collection("records").deleteOne(myquery, function (err, obj) {
+        if (err) throw err;
+    });
+});
+
+module.exports = app;
